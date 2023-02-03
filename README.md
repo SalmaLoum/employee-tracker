22e7c13663d9cf06c1e24199600f3a3b8a729bb7
salmaloum <goforsalma@gmail.com>
2023-02-02 16:48:59 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # employee-tracker
+
+## Badge
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-informational.svg)](https://opensource.org/licenses/MIT)
+
+## Description 
+This **content management systems (CMS)** is built through a command-line application to manage a company's employee database, using `Node.js`, `Inquirer`, and `MySQL`.
+
+----
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Instructions](#instructions)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+  
+## Installation
+
+Run node index.js in terminal.
+
+----
+
+## Instructions
+
+- [Walkthrough video]()
+
+Follow the instructions below to start the app using your command line:
+
+- ![Command line](./assets/)
+
+Follow the instructions below to check your database through My SQL Workbench
+
+- ![MySql](./assets/)
+
+  
+## Usage
+
+- AS A business owner
+- I WANT to be able to view and manage the departments, roles, and employees in my company
+- SO THAT I can organize and plan my business
+
+---- 
+  
+## Credits
+- [My SQL Workbench](https://www.mysql.com/products/workbench/)
+- [UCLA Extension Bootcamp](https://www.uclaextension.edu/?gclid=Cj0KCQiAgribBhDkARIsAASA5btdbwAz8x25r3b1deoRNIGxfkPFL11rAQMuCgQ7HYiqBH8CLr9CgLoaAktlEALw_wcB&gclsrc=aw.ds)
+
+----
+
+## License
+MIT License
+
+Copyright (c) 2023 Sara Angelo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ ----
+
+## Questions
+
+If you have any questions about the repo you can open an issue on this repo, or reachout to me via 
+my [Github](https://www.github.com/salmaLoum)
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const connection = require('./db/connection');
-const mysql = require('mysql2');
-const { prompt } = require('inquirer');
-const consoleTable = require('console.table');
-const queries = require('./db/Queries');
-//const { prototype } = require('events');
+const connection = require('./db/connection')
+const mysql = require('mysql2')
 
+const { prompt } = require('inquirer')
+
+const consoleTable = require('console.table')
+const Queries = require('./db/Queries')
 
 class Queries {
   constructor(connection) {
@@ -31,8 +31,7 @@ class Queries {
     FROM employee LEFT JOIN roles on employee.role_id = roles.id
     LEFT JOIN department on roles.department_id = departments.id
     LEFT JOIN employee manager on manger.id = employee.manager_id;
-    `
-    )
+    `)
   }
 
   findAllRoles() {
@@ -41,70 +40,75 @@ class Queries {
       department.name AS department, 
       roles.salary 
       FROM roles 
-      LEFT JOIN department on roles.department_id = department.id;`
-    );
+      LEFT JOIN department on roles.department_id = department.id;`,
+    )
   }
-  
+
   findAllPossibleManagers(employeeId) {
-    return this.connection.promise().query(
-      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
-      employeeId
-    );
+    return this.connection
+      .promise()
+      .query(
+        'SELECT id, first_name, last_name FROM employee WHERE id != ?',
+        employeeId,
+      )
   }
 
   createEmployee(employee) {
-    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    return this.connection
+      .promise()
+      .query('INSERT INTO employee SET ?', employee)
   }
 
-  
   removeEmployee(employeeId) {
-    return this.connection.promise().query(
-      "DELETE FROM employee WHERE id = ?",
-      employeeId
-    );
+    return this.connection
+      .promise()
+      .query('DELETE FROM employee WHERE id = ?', employeeId)
   }
 
   updateEmployeeRole(employeeId, roleId) {
-    return this.connection.promise().query(
-      "UPDATE employee SET role_id = ? WHERE id = ?",
-      [roleId, employeeId]
-    );
+    return this.connection
+      .promise()
+      .query('UPDATE employee SET role_id = ? WHERE id = ?', [
+        roleId,
+        employeeId,
+      ])
   }
 
   findAllRoles() {
-    return this.connection.promise().query(
-      "SELECT roles.id, roles.title, department.name AS department, roles.salary FROM roles LEFT JOIN department on roles.department_id = department.id;"
-    );
+    return this.connection
+      .promise()
+      .query(
+        'SELECT roles.id, roles.title, department.name AS department, roles.salary FROM roles LEFT JOIN department on roles.department_id = department.id;',
+      )
   }
 
   createRole(role) {
-    return this.connection.promise().query("INSERT INTO roles SET ?", role);
+    return this.connection.promise().query('INSERT INTO roles SET ?', role)
   }
 
   removeRole(roleId) {
-    return this.connection.promise().query("DELETE FROM roles WHERE id = ?", roleId);
+    return this.connection
+      .promise()
+      .query('DELETE FROM roles WHERE id = ?', roleId)
   }
 
- 
   findAllDepartments() {
-    return this.connection.promise().query(
-      "SELECT departments.id, departments.name FROM department;"
-    );
+    return this.connection
+      .promise()
+      .query('SELECT departments.id, departments.name FROM department;')
   }
 
   createDepartment(department) {
-    return this.connection.promise().query("INSERT INTO department SET ?", department);
+    return this.connection
+      .promise()
+      .query('INSERT INTO department SET ?', department)
   }
 
-  
   removeDepartment(departmentId) {
-    return this.connection.promise().query(
-      "DELETE FROM department WHERE id = ?",
-      departmentId
-    );
+    return this.connection
+      .promise()
+      .query('DELETE FROM department WHERE id = ?', departmentId)
   }
-
-
 }
 
 module.exports = new Queries(connection)
